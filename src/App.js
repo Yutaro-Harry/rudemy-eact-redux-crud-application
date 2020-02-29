e7646375fde79351ecc522684eeba95ef630658c
YutaroSuwaki <Dominic.mem@gmail.com>
2020-03-01 01:27:58 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, {Component} from 'react';
 
-class App extends Component {
-  render () {
-    // const greeting = "qwe"
-    // const dom =  <h1 className="foo">{greeting} Hello, world!</h1>
-    // return dom
-    return (
-      <React.Fragment>
-        <label htmlFor= "bar">bar</label>
-        <input type="text" onChange={() => {console.log("aaa")}}/>
-      </React.Fragment>
-    )
-  }
-}
+// class App extends Component {
+//   render () {
+//     // const greeting = "qwe"
+//     // const dom =  <h1 className="foo">{greeting} Hello, world!</h1>
+//     // return dom
+//     return (
+//       <React.Fragment>
+//         <label htmlFor= "bar">bar</label>
+//         <input type="text" onChange={() => {console.log("aaa")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
 
+const App =()=>{
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+const Cat =()=>{
+  return <div>Hi!</div>
+}
 
 export default App;
