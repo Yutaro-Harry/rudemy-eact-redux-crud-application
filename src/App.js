d4748bef7faf157091060f16bc1b72a89aa1d741
YutaroSuwaki <Dominic.mem@gmail.com>
2020-03-01 01:15:24 +0900

    もっとjsx

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React, {Component} from 'react';
 
 class App extends Component {
   render () {
-    return <div><h1>Hello, world</h1></div>
+    // const greeting = "qwe"
+    // const dom =  <h1 className="foo">{greeting} Hello, world!</h1>
+    // return dom
+    return (
+      <React.Fragment>
+        <label htmlFor= "bar">bar</label>
+        <input type="text" onChange={() => {console.log("aaa")}}/>
+      </React.Fragment>
+    )
   }
 }
+
 
 export default App;
